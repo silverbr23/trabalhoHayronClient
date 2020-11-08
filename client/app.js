fae5e3f48ca1165246159a76201590b5d9a27be7
Rafael Caitano <rafael.caitano@leanonsystems.com>
2020-11-07 22:45:20 -0300

    Q coisa linda

--- client/app.js
+++ client/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -10,6 +11,7 @@ var checkoutRouter = require('./routes/checkout');
 
 var app = express();
 
+  
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -17,6 +19,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- client/routes/checkout.js
+++ client/routes/checkout.js
@@ -3,9 +3,50 @@ var router = express.Router();
 const fetch = require('node-fetch');
 
 
-router.get('/', function (req,res,next){
-  
-    res.send(req.query.produto)
+
+const middle = (req, res, next) => {
+    fetch('https://trabalho-hayron.herokuapp.com/api/produto/getSpecificProduct', {
+            method: 'POST',
+            body: JSON.stringify({
+                "name": req.body.name
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(res => res.json())
+        .then(json => {
+
+            if (json.result) {
+                req.resultadoQ = json.result
+                next()
+
+            } else {
+                res.send("no products")
+            }
+        })
+}
+
+router.post('/', function (req, res, next) {
+    console.log(req.body)
+    res.render('checkout', {
+        produto: req.body
+    })
+})
+
+router.post('/confirm', middle, function (req, res) {
+    
+    lojaLocal = "U002"
+    produtoTemLocal = req.resultadoQ.resultFilial.some((a) => a.identificador == lojaLocal)
+    console.log(produtoTemLocal)
+
+
+    res.render('confirmation', {
+        produto: req.resultadoQ.resultProd[0],
+        prodLocal: produtoTemLocal
+
+    })
+
+
 })
 
 module.exports = router;
